76dc6311e9f6dcaf34f321bac37027e10f8ca1b8
Matt <matthv@gmail.com>
2021-12-14 14:08:32 +0100

    chore: update config

--- config/forest.php
+++ config/forest.php
@@ -2,10 +2,11 @@
 
 return [
     'models_directory' => env('MODEL_DIRECTORY', 'app/Models/'),
+    'models_namespace' => env('MODEL_NAMESPACE', 'App\Models\\'),
     'json_file_path'   => env('JSON_FILE_PATH', '.forestadmin-schema.json'),
     'api'              => [
         'url'         => env('FOREST_URL', 'https://api.development.forestadmin.com'),
-        'secret'      => '586f9a1c0f6a443bd590386c07136e17dbabd85ecf029d7b6ab51e912b21425a',
+        'secret'      => 'a33754d559dfec688726d147bbc4ff6c36eacdd771e572876a0b54deae9785e3',
         'auth-secret' => 'GIC0R-MFcemxnZEBJz8DxQ',
     ]
 ];
