b715e96747cba831ca85a7b72651c9539fcf3f4a
Matt <matthv@gmail.com>
2021-12-27 11:50:39 +0100

    chore: update model category

--- database/migrations/2021_10_14_132205_create_categories_table.php
+++ database/migrations/2021_10_14_132205_create_categories_table.php
@@ -16,7 +16,6 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('label');
-            $table->foreignId('product_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
