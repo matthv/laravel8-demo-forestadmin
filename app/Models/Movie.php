7697d3f789dd9f89be7cb5bab4292f4310ca8e5f
Nicolas Alexandre <nicolasalexandre9@gmail.com>
2022-03-24 11:46:32 +0100

    chore: added smart-relationship "belongsTo" example

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -2,13 +2,38 @@
 
 namespace App\Models;
 
+use ForestAdmin\LaravelForestAdmin\Services\Concerns\ForestCollection;
+use ForestAdmin\LaravelForestAdmin\Services\SmartFeatures\SmartField;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Movie extends Model
 {
-    use HasFactory;
+    use HasFactory, ForestCollection;
+
+    /**
+     * @return SmartField
+     */
+    public function bookCategory(): SmartField
+    {
+        return $this->smartField(
+            [
+                'type' => 'String',
+                'reference' => 'category.id'
+            ]
+        )
+            ->get(
+                function () {
+                    $category = Category::select('categories.*')
+                        ->join('books', 'books.category_id', '=', 'categories.id')
+                        ->where('books.id', $this->book_id)
+                        ->first();
+
+                    return $category;
+                }
+            );
+    }
 
     /**
      * @return BelongsTo
