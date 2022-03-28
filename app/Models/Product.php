04cd35074359f7964a4aaaf46a37ebb48cafba38
Matt <matthv@gmail.com>
2022-03-28 12:05:00 +0200

    fix(product): remove categories relation

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\App;
 
@@ -170,10 +169,5 @@ class Product extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
-    }
-
-    public function categories(): HasMany
-    {
-        return $this->hasMany(Category::class);
     }
 }
